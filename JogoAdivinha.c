19289cb80fb9ba6b3dcecfb1f4f0ff2b554db7fc
PVOdeOliveira <p.vitor.oliveira.pvo@gmail.com>
2022-02-27 20:10:53 -0300

    Recebendo a tentativa do usuario

--- JogoAdivinha.c
+++ JogoAdivinha.c
@@ -2,11 +2,15 @@
 #include<stdio.h>
 
 int main(){
-    int numSecreto = 33;
+    int numSecreto = 33, chute;
 
 	printf("******************************************\n");
 	printf("* Bem Vindo ao nosso jogo de adivinhacao *\n");
 	printf("******************************************\n\n");
+
+	printf("Voce deve adivinhar o numero:\n");
+	printf("Digite o seu chute: ");
+	scanf("%d",&chute);
 
 	return 0;
 }
