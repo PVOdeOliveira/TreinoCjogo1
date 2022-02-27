d3a9200ea211bf677689f77fba1364d355840b68
PVOdeOliveira <p.vitor.oliveira.pvo@gmail.com>
2022-02-27 20:35:21 -0300

    Testando o chute do usuario

--- JogoAdivinha.c
+++ JogoAdivinha.c
@@ -12,5 +12,15 @@ int main(){
 	printf("Digite o seu chute: ");
 	scanf("%d",&chute);
 
+	if(chute == numSecreto){
+        printf("Parabens! Voce acertou o numero!\n");
+	}
+	else if(chute < numSecreto){
+        printf("O numero secreto e maior que %d!\n",chute);
+	}
+	else{
+        printf("O numero secreto e menor que %d!\n",chute);
+	}
+
 	return 0;
 }
