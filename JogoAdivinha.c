42ffacee200f29e9920766153b958f72d4ef64d2
PVOdeOliveira <p.vitor.oliveira.pvo@gmail.com>
2022-02-27 20:07:21 -0300

    Adicionando o numero secreto (fixo)

--- JogoAdivinha.c
+++ JogoAdivinha.c
@@ -2,6 +2,8 @@
 #include<stdio.h>
 
 int main(){
+    int numSecreto = 33;
+
 	printf("******************************************\n");
 	printf("* Bem Vindo ao nosso jogo de adivinhacao *\n");
 	printf("******************************************\n\n");
